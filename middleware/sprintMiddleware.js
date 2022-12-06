41d919fbdaf380599245f65a8c255b177122b7e5
EvgenYakov <82239759+EvgenYakov@users.noreply.github.com>
2022-12-06 13:44:11 +0300

    fix change status

--- middleware/sprintMiddleware.js
+++ middleware/sprintMiddleware.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const Tokens = require("../models/tokens");
 const Sprint = require("../models/sprint");
+const Task = require("../models/task")
 
 const checkExp = asyncHandler(async (req,res,next)=>{
         try {
@@ -18,7 +19,25 @@ const checkExp = asyncHandler(async (req,res,next)=>{
             throw new Error()
         }
 })
+const checkExpWithTask = asyncHandler(async (req,res,next)=>{
+    try {
+        const task = await Task.findById(req.params.id)
+        const sprint = await Sprint.findById(task.sprintId)
+        if (sprint.dateEnd < Date.now()){
+            throw new Error("Спринт просрочен")
+        }
+        if (sprint.status < "complete"){
+            throw new Error("Спринт завершен")
+        }
+        next()
+    }catch (e) {
+        res.status(400)
+        throw new Error()
+    }
+})
+
 
 module.exports = {
-    checkExp
+    checkExp,
+    checkExpWithTask
 }
--- routes/task.js
+++ routes/task.js
@@ -2,13 +2,14 @@ const Router = require('express')
 const {register} = require("../controllers/authController");
 const {protect} = require("../middleware/authMiddleware");
 const {getTasks, addTask,changeStatus,editTask, deleteTask} = require("../controllers/taskController");
+const {checkExpWithTask} = require("../middleware/sprintMiddleware");
 const router = Router()
 
 
 router.route("/:id").get(protect, getTasks).post(protect,addTask)
 router.route("/:id").put(protect, editTask).delete(protect,deleteTask)
 
-router.route("/changeStatus/:id").patch(protect,changeStatus)
+router.route("/changeStatus/:id").patch(protect,checkExpWithTask,changeStatus)
 
 
 module.exports = router
